657b1191351e4d56e4bdee60805b114473496b78
Istoshin Pavlo <pavloistoshin@gmail.com>
2025-12-12 17:32:00 +0200

    Update settings, add Redis config.

--- src/core/config.py
+++ src/core/config.py
@@ -17,11 +17,37 @@ class DBConfig(BaseSettings):
         return f"postgresql+asyncpg://{self.user}:{self.password}@{self.host}:{self.port}/{self.name}"
 
 
+class RedisConfig(BaseSettings):
+    host: str = "localhost"
+    port: int = 6379
+    db: int = 0
+
+    model_config = SettingsConfigDict(
+        env_file=".env", env_prefix="REDIS_", extra="ignore"
+    )
+
+class AuthorsNamespace(BaseSettings):
+    authors_list: str = "authors_list"
+    author: str = "author"
+
+class BooksNamespace(BaseSettings):
+    books_list: str = "books_list"
+    book: str = "book"
+
+class CacheNamespace(BaseSettings):
+    authors: AuthorsNamespace = AuthorsNamespace()
+    books: BooksNamespace = BooksNamespace()
+
+
+class CacheConfig(BaseSettings):
+    prefix: str = "cache"
+    namespace: CacheNamespace = CacheNamespace()
 
 
 class Settings(BaseSettings):
     db: DBConfig = DBConfig()
-
+    redis: RedisConfig = RedisConfig()
+    cache: CacheConfig = CacheConfig()
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
 
